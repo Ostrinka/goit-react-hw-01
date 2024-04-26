dfcc12f6a04fe56bb4dc02b4776b32952dc8fcb3
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-04-26 17:40:21 +0300

    add FriendList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Profile from './components/Profile/Profile';
 import userData from './userData.json';
 
+import FriendList from './components/FriendList/FriendList';
+import friends from './friends.json'
+
 export default function App() { 
   return (
     <>
-    <Profile userData = {userData}/>
+      <Profile userData={userData} />
+      <FriendList friends={friends}/>
     </>  
   )
 }
