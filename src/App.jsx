17f254a06f1c6fd14558a6c0782b180b3cc34541
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-04-27 23:02:30 +0300

    add Transaction

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import userData from './userData.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './friends.json'
 
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from "./transactions.json";
+
 export default function App() { 
   return (
     <>
       <Profile userData={userData} />
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>  
   )
 }
